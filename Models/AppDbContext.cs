64a4f183b6b0e5b220974c3da3897d5588d3abd4
shrirang010 <shrirangthatsit10@gmail.com>
2024-07-07 22:22:19 +0530

    Added Entity and AppDbcontext class for Database

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Product_Inventory_Management_System.Models
+{
+    public class AppDbContext :DbContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_Inventory_Management_System.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [MaxLength(100)]
+        public string Category{ get; set; } = "";
+        public decimal Price { get; set; }  
+        public int StockQuantity { get; set; }  
+
+
+    }
+}
